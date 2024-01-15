c29497672c3d7ebf10e6cf07f563b0115de07915
Fatullayev Mahmud Haji <fatullayev_m@itstep.edu.az>
2024-01-15 21:41:18 +0400

    state props

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import User from './components/Card/User'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,29 @@
 import './index.css'
 import List from './final-products.json'
-import React from 'react'
-import Card from '../Card/Card'
+import React, { useState } from 'react'
+import Cart from '../Card/Card'
+
+function getStore(){
+    return JSON.parse(localStorage.getItem('store'))?? []
+}
 
 export default function Navbar(){
-    console.log(List)
+    const [list, setList] = useState(getStore())
+    console.log(list)
+
+    const handleShoppingBag = (el) => {
+        console.log(el)
+        let store = (JSON.parse(localStorage.getItem('store')) || [])
+        store.push(el)
+        setList((prev) => [...prev, el])
+        localStorage.setItem('store', JSON.stringify(store))
+    }
     return (
+        <>
+        <p>Count {list.length}</p>
         <div className='container'>
-            {List.map(el => <Card key={el.id} el={el} active={false}/>)}
+            {List.map((el, index) => <Cart key={index} el={el} active={false} handler={handleShoppingBag}/>)}
         </div>
+        </>
     )
 }
